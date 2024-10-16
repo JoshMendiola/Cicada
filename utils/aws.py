624509d82514a2578bec8ffe5790c5f26f6f0ef7
PhilosophyWithJosh <joshmendiola@icloud.com>
2024-10-16 12:07:38 -0500

    remove AWS files

--- utils/aws.py
+++
@@ -1,9 +0,0 @@
-from utils.config import s3_client, S3_BUCKET_NAME
-
-
-def download_from_s3(s3_path, local_path):
-    s3_client.download_file(S3_BUCKET_NAME, s3_path, local_path)
-
-
-def upload_to_s3(local_path, s3_path):
-    s3_client.upload_file(local_path, S3_BUCKET_NAME, s3_path)
